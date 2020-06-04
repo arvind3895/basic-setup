b4582d490a382acd188d20a878d92626465c8662
Rafter Aravind Krishna Kotha <racinfrallp@RACs-MacBook-Pro.local>
2020-06-05 02:11:41 +0530

    css-file added

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link, BrowserRouter as Router } from "react-router-dom";
+import "./nav.css";
 
 class Nav extends React.Component {
   render() {
     return (
       <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
+        <nav className="nav-bar">
+          <ul className="nav-items-list">
+            <li className="nav-items">
+              <Link to="/home">Home</Link>
             </li>
-            <li>
+            <li className="nav-items">
               <Link to="/about">About</Link>
             </li>
-            <li>
-              <Link to="/users">Users</Link>
+            <li className="nav-items">
+              <Link to="/contact">Contact</Link>
             </li>
           </ul>
         </nav>
