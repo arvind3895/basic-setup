7d1fed994cd9943003c548457765ccb393410464
Rafter Aravind Krishna Kotha <racinfrallp@RACs-MacBook-Pro.local>
2020-06-06 01:50:03 +0530

    axios added

--- src/component/home.js
+++ src/component/home.js
@@ -1,8 +1,34 @@
 import React from "react";
+import axios from "axios";
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res);
+      this.setState({
+        userDetails: res.data,
+      });
+    });
+  }
+  userList() {
+    return this.state.userDetails.map((item) => {
+      return <li>{item.name}</li>;
+    });
+  }
+
   render() {
-    return <div>Home</div>;
+    var userExist = this.state.userDetails;
+    return (
+      <div>
+        Home
+        {userExist ? <ul>{this.userList()}</ul> : "no users exist"}
+      </div>
+    );
   }
 }
 export default Home;
